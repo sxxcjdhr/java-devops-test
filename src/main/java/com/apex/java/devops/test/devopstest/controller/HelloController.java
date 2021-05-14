6cab0247934b49850bce81cf9ba1380ce21d0efa
xiuxian.song <xiuxian.song@apexglobe.com>
2021-05-14 16:08:12 -0700

    init

---
+++ src/main/java/com/apex/java/devops/test/devopstest/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.apex.java.devops.test.devopstest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xiuxian.song
+ * @version 1.0
+ * @create: 2021-05-14 15:58
+ */
+@RestController
+public class HelloController {
+  @RequestMapping("/")
+  public String Hello(){
+      String msg = "This is from apex devops test";
+      System.out.println(msg);
+      return msg;
+  }
+}
