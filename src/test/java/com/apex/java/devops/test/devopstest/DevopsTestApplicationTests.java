078b1dfbd1cf9b73c96de7fe357de76a3cd7c883
xiuxian.song <xiuxian.song@apexglobe.com>
2021-05-17 01:38:08 -0700

    new batch

--- src/test/java/com/apex/java/devops/test/devopstest/DevopsTestApplicationTests.java
+++ src/test/java/com/apex/java/devops/test/devopstest/DevopsTestApplicationTests.java
@@ -1,5 +1,6 @@
 package com.apex.java.devops.test.devopstest;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class DevopsTestApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.assertTrue(false);
 	}
 
 }
--- src/test/java/com/apex/java/devops/test/devopstest/controller/HelloControllerTest.java
+++ src/test/java/com/apex/java/devops/test/devopstest/controller/HelloControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.After;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /** 
@@ -17,8 +18,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 * @since <pre>5? 16, 2021</pre> 
 * @version 1.0 
 */
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = DevopsTestApplication.class)
 public class HelloControllerTest { 
 
 @Before
