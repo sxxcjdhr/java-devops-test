796003f7bd78c01c9824a4da09d40fc156d74887
xiuxian.song <xiuxian.song@apexglobe.com>
2021-05-17 23:43:19 -0700

    new batch

--- src/main/java/com/apex/java/devops/test/devopstest/controller/HelloController.java
+++ src/main/java/com/apex/java/devops/test/devopstest/controller/HelloController.java
@@ -14,9 +14,9 @@ public class HelloController {
   public String Hello(boolean flag){
       String msg;
       if (flag){
-          msg = "This is from apex devops test, abc, flag is on";
+          msg = "This is from apex devops test, batch1, flag is on";
       }else{
-          msg = "This is from apex devops test, abc, flag is off";
+          msg = "This is from apex devops test, batch1, flag is off";
       }
       System.out.println(msg);
       return msg;
